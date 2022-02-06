a33197e9285c5e1905c7bac6ee5ea137f842ae97
takuya86 <takuya86takuya@gmail.com>
2022-02-06 23:23:06 +0900

    新規登録実装中2/6

--- backend/app/controllers/api/v1/auth/registrations_controller.rb
+++ backend/app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+  binding.pry
   private
 
     def sign_up_params
-      params.permit(:name, :email, :password)
+      params.require(:registration).permit(:name,:email, :password)
     end
 end
