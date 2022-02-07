bd9dddc2225483c6022a86aebda23811f1e9ed6b
takuya86 <takuya86takuya@gmail.com>
2022-02-07 23:17:25 +0900

    stagingコミット

--- frontend/layouts/header.vue
+++ frontend/layouts/header.vue
@@ -7,7 +7,7 @@
       <v-spacer />
       <v-btn v-if="!this.$auth.loggedIn" color="#FFF" plain to="/users/login">ログイン</v-btn>
       <v-btn v-if="!this.$auth.loggedIn" color="#FFF" plain to="/users/signup">新規登録</v-btn>
-      <v-btn v-if="!this.$auth.loggedIn" color="#FFF" plain>マイページ</v-btn>
+      <v-btn v-if="this.$auth.loggedIn" color="#FFF" plain>マイページ</v-btn>
       <v-btn v-if="this.$auth.loggedIn" rounded color="primary" dark>＋目標追加</v-btn>
     </v-app-bar>
   </header>
