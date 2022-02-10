21477bb78ffa5a4fcb64545f9887e5549891203e
takuya86 <takuya86takuya@gmail.com>
2022-02-10 21:28:23 +0900

    新規登録実装完了

--- frontend/pages/users/signup.vue
+++ frontend/pages/users/signup.vue
@@ -61,6 +61,7 @@ export default {
         })
       } catch (e) {
         console.log(e), (this.error = e.response.data.errors.full_messages)
+        console.log(e.response.data)
       }
     },
   },
