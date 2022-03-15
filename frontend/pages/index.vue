0e248484b797b05137262dbedbfa17483c0a7f5e
takuya86 <takuya86takuya@gmail.com>
2022-03-15 23:41:40 +0900

    習慣化目標に親から子にデータ受け渡し

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,9 +1,7 @@
 <template>
   <v-container>
-    <h1>ハビット！</h1>
-    <p>習慣化を記録するアプリです。</p>
-    <h2>ログイン状態:{{ $auth.loggedIn }}</h2>
-    <h2>ユーザー情報:{{ $auth.user }}</h2>
+    <h1>Habi appで学習を習慣化しよう。</h1>
+    <p>習慣化目標を記録するアプリです。</p>
     <div>
       <HabitationsCard  v-for=" habitation in habitations" :key="habitation.id" :habitation="habitation" />
     </div>
@@ -23,12 +21,6 @@ export default {
       habitations: [],
     }
   },
-  // props: {
-  //   habitations: {
-  //     type: Object,
-  //     required: true,
-  //   },
-  // },
   created() {
     axios.get(`http://localhost:5000/api/v1/habitations`)
       .then(res => {
