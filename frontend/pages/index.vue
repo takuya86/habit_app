05c355cc3b763155628740275d7f1084b7c52955
takuya86 <takuya86takuya@gmail.com>
2022-03-19 18:55:42 +0900

    ユーザーデータの確認

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,7 +2,6 @@
   <v-container>
     <h1>Habi appで学習を習慣化しよう。</h1>
     <p>習慣化目標を記録するアプリです。</p>
-    <p>{{ $store.state.user.data.data }}</p>
     <div>
       <HabitationsCard  v-for=" habitation in habitations" :key="habitation.id" :habitation="habitation" />
     </div>
